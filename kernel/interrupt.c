5acfc8580220b44d6e7313f5a3d3635f02b73857
nightmare-man <2596204805@qq.com>
2020-09-09 15:17:20 +0800

    add pic init code

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -8,8 +8,14 @@
 #include "../lib/kernel/stdint.h"
 #include "../lib/kernel/global.h"
 #include "../lib/kernel/print.h"
+#include "../lib/kernel/io.h"
 //#include "global.h"
 #define IDT_DESC_CNT 0x21 //目前支持的中断数
+
+#define PIC_M_CTRL 0x20 //8259a主片 控制 端口
+#define PIC_M_DATA 0x21 //数据端口
+#define PIC_S_CTRL 0xa0 //从片 控制端口
+#define PIC_S_DATA 0xa1 //从片数据端口
 
 //以下是中断门描述符格式 共8字节
 struct gate_desc{
@@ -36,11 +42,30 @@ static void idt_desc_init(void){
     }
     put_str("    idt_desc_init done!\n");
 }
+static void pic_init(void){
 
+    //初始化主片
+    outb(PIC_M_CTRL,0x11);//ICW1 边沿触发 级联  使用icw4
+    outb(PIC_M_DATA,0x20);//ICW2 PIC控制的中断号的起始地址 0x20 (32起 0-19cpu用了 20-31保留了 
+    //，而且起始地址 只能设置高5位 必须为8的倍数)
+    outb(PIC_M_DATA,0x04);//ICW3 主片IR2接从片 0000 0100
+    outb(PIC_M_DATA,0x01);//不自动结束中断 8086模式
+
+    //初始化从片
+    outb(PIC_S_CTRL,0x11);
+    outb(PIC_S_DATA,0x28);//从片的中断号 延续主片
+    outb(PIC_S_DATA,0x02);//告诉从片字节被链接到了主片的IR2上
+    outb(PIC_S_DATA,0x01);
+
+    //只开放主片IR0 （连在时钟上，时钟中断）
+    outb(PIC_M_DATA,0xfe);//OCW1 完成ICW初始化后 对0x21/0xa1 的第一个写入即是OCW1 用于屏蔽IR 
+    outb(PIC_S_DATA,0Xff);//从片全关
+    put_str("   pic_init done!\n");
+}
 void idt_init(){
     put_str("idt_init start\n");
     idt_desc_init();//初始化中断表
-  //  pic_init();//初始化8259a
+    pic_init();//初始化8259a
     uint64_t idt_operand = (sizeof(idt)-1)|(  ((uint64_t)((uint32_t)idt) )<<16  );//sizeof(idt)-1即是idt界限
     asm volatile("lidt %0"::"m"(idt_operand));
     put_str("idt_init done!\n");
