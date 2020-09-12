97b7aea1fcf69023907dba99321c10f531613192
nightmare-man <2596204805@qq.com>
2020-09-12 10:41:06 +0800

    fixed build script with timer.c's path

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,7 +1,7 @@
 gcc -c -m32 -fno-stack-protector main.c -o ../build/main.o
 gcc -c -m32 -fno-stack-protector init.c -o ../build/init.o
 gcc -c -m32 -fno-stack-protector interrupt.c -o ../build/interrupt.o
-gcc -c -m32 -fno-stack-protector timer.c -o ../build/timer.o
+gcc -c -m32 -fno-stack-protector ../device/timer.c -o ../build/timer.o
 nasm -f elf32 kernel.s -o ../build/kernel.o
 nasm -f elf32 print.s -o ../build/print.o
 ld ../build/main.o ../build/init.o ../build/interrupt.o ../build/kernel.o ../build/print.o ../build/timer.o -melf_i386 -Ttext 0xc0001500 -e main -o ../build/kernel.bin
